2be705532f24eef153c1f587bb56fb18025bb5f5
matcha <massahiro.endo@gmail.com>
2022-06-27 18:58:18 +0200

    feat Link to NavLink for HomePage Logo display

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,18 @@
 import { FC } from 'react';
 import { Flex } from '@chakra-ui/react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
+import styles from './styles.module.scss';
 
 const Layout: FC = () => {
   return (
     <Flex direction="column">
       <Header />
       <main>
-        <Link to="">GOLD and SILVER</Link>
+        <NavLink to="" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+          GOLD and SILVER
+        </NavLink>
         <Outlet />
       </main>
       <Footer />
