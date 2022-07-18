8a46ad7dde4d18e97469ce200c7d0c6c629a33d0
matcha <massahiro.endo@gmail.com>
2022-07-18 20:58:48 +0200

    feat: add colorModeValue from chakra-ui in footer themetoggle

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,17 @@
-import { Box } from "@chakra-ui/react";
-import { FC } from "react";
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { FC } from 'react';
 
-type Props = {}
+type Props = {};
 
 const Footer: FC<Props> = () => {
+  const footerBg = useColorModeValue('#f0d7a5', '#191F22');
   return (
-    <footer><Box bg="orange.200" p={4}>Pied de nez</Box></footer>
-  )
-}
+    <footer>
+      <Box bg={footerBg} p={4}>
+        Pied de nez
+      </Box>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
