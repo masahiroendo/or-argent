b1b05aa687c2f9e37e7a96889e7e46e503edbded
matcha <massahiro.endo@gmail.com>
2022-07-18 23:18:52 +0200

    feat: fetch data from metal-api (open and close prices) and inject into the jsx.

--- src/components/Header/SpotPrice/index.tsx
+++ src/components/Header/SpotPrice/index.tsx
@@ -25,7 +25,7 @@ const SpotPrice: FC<SpotPriceProps> = (props) => {
 
   return (
     <HStack className={style['spot-price']}>
-      <span>{props.asset}</span> <span>{props.price}</span>
+      <span>{props.asset}</span> <span>{props.price.toFixed(2)}</span>
       <span>{props.symbol}</span>{' '}
       {variation < 0 ? <ArrowDownIcon viewBox="0 0 24 24" color="red" /> : <ArrowUpIcon color="green" />}
       <span className={style[variationStyle]}>{variation.toPrecision(2)}%</span>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,71 @@
+import { FC, useEffect, useState } from 'react';
 import { Box, Button, ButtonGroup, Flex, Show, Spacer } from '@chakra-ui/react';
-import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import CurrencySelect from './CurrencySelect';
 import LanguageSelect from './LanguageSelect';
 import SpotPrice from './SpotPrice';
 import styles from './style.module.scss';
 import { ThemeSwitchBtn } from './themeSwitchBtn';
+import { makeMetalPricesApiOHLCEndpoint } from '../../constants/endpoints';
+import { MetalsAPIOHLCResponse } from '../../constants/apiResponses';
+
+type SpotPriceDataType = {
+  price: number;
+  open: number;
+};
+
+const initSpotPrice: SpotPriceDataType = {
+  open: 0,
+  price: 0,
+};
+
+const fetchLatestGoldAndSilverPrices = async (): Promise<{ gold: SpotPriceDataType; silver: SpotPriceDataType }> => {
+  const goldResponse = await fetch(makeMetalPricesApiOHLCEndpoint('XAU'));
+  const goldData = (await goldResponse.json()) as MetalsAPIOHLCResponse;
+  const silverResponse = await fetch(makeMetalPricesApiOHLCEndpoint('XAG'));
+  const silverData = (await silverResponse.json()) as MetalsAPIOHLCResponse;
+
+  if (!goldData.success || !silverData.success) {
+    throw Error('Could not fetch data from Metals-API');
+  }
+
+  return {
+    gold: {
+      open: goldData?.rates?.open,
+      price: goldData?.rates?.close,
+    },
+    silver: {
+      open: silverData?.rates?.open,
+      price: silverData?.rates?.close,
+    },
+  };
+};
 
 const Header: FC = () => {
+  const [goldSpot, setGoldSpot] = useState<SpotPriceDataType>(initSpotPrice);
+  const [silverSpot, setSilverSpot] = useState<SpotPriceDataType>(initSpotPrice);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { gold, silver } = await fetchLatestGoldAndSilverPrices();
+        setGoldSpot(gold);
+        setSilverSpot(silver);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Flex bg="grey.100" paddingX={{ base: '10px', md: '25px', lg: '75px' }}>
       <Box display="flex" alignItems="center" gap={2}>
         <NavLink to="Gold" className={({ isActive }) => (isActive ? styles.active : undefined)}>
-          <SpotPrice asset={'OR'} price={1783.85} symbol={'€'} openPrice={1780} />
+          <SpotPrice asset={'OR'} price={goldSpot.price} symbol={'€'} openPrice={goldSpot.open} />
         </NavLink>
         <NavLink to="Silver" className={({ isActive }) => (isActive ? styles.active : undefined)}>
-          <SpotPrice asset={'ARGENT'} price={21.25} symbol={'€'} openPrice={22} />
+          <SpotPrice asset={'ARGENT'} price={silverSpot.price} symbol={'€'} openPrice={silverSpot.open} />
         </NavLink>
       </Box>
       <Spacer />
