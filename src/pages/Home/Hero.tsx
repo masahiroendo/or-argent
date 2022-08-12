0f6ec245b663f7f3eb7fac77badc8c12f48c3bd1
matcha <massahiro.endo@gmail.com>
2022-08-13 00:07:11 +0200

    fix: In Hero modified responsive styles
    - Retrieve ButtonGroup
    - Box to Flex w/ some css
    - Container  adjust css px & py

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Box, Button, ButtonGroup, Container, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, Heading } from '@chakra-ui/react';
 import { FaPlayCircle } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 
@@ -7,21 +7,19 @@ const Hero: FC = () => {
   const { t } = useTranslation(['translation', 'home']);
 
   return (
-    <Container pt={'3em'} pl={{ md: '17em' }} pb={'5em'} pr={{ md: '17em' }} maxW="container.xl">
+    <Container py={'3em'} px={{ md: '10em', lg: '17em' }} maxW="container.xl">
       <Heading as="h1" size="3xl" mb={'1em'}>
         {t('title', { ns: 'home' })}
       </Heading>
       <Box>{t('description', { ns: 'home' })}</Box>
-      <Box>
-        <ButtonGroup alignItems={'center'} my={'2em'}>
-          <Button size="lg" variant="ghost" border="1px" borderColor="yellow.500" borderRadius="5em">
-            {t('create-account')}
-          </Button>
-          <Button leftIcon={<FaPlayCircle />} size="lg" borderRadius="5em" _before={{ opacity: '85%' }}>
-            {t('watch-a-demo')}
-          </Button>
-        </ButtonGroup>
-      </Box>
+      <Flex my={'2em'} gap={{ base: 4, sm: 2 }} direction={{ base: 'column', sm: 'row' }}>
+        <Button size="lg" variant="ghost" border="1px" borderColor="yellow.500" borderRadius="5em">
+          {t('create-account')}
+        </Button>
+        <Button leftIcon={<FaPlayCircle />} size="lg" borderRadius="5em" _before={{ opacity: '85%' }}>
+          {t('watch-a-demo')}
+        </Button>
+      </Flex>
     </Container>
   );
 };
