17e245259dcbf1696befd38eedcc2dca1aaa7af0
matcha <massahiro.endo@gmail.com>
2022-08-19 15:04:50 +0200

    add products & images in Products index

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -5,6 +5,7 @@ type ProductFamily = 'coin' | 'bar';
 type MetalType = 'gold' | 'silver' | 'palladium' | 'platinum';
 
 export const OUNCE_WEIGHT = 31.1035;
+export const BAR_WEIGHT = 1000.0;
 export const PRODUCTS_IMAGES_PATH = '/assets/images/products/';
 
 export type ProductSpec = {
@@ -32,10 +33,32 @@ export type Product = {
 export const storeProducts: Product[] = [
   {
     id: faker.unique(v4),
+    type: 'bar',
+    metal: 'gold',
+    name: '1 kilogram Gold Bar Valcambi',
+    slug: '1kg-gold-bar-valcambi',
+    description: faker.lorem.lines(5),
+    price: parseInt(faker.finance.amount(1200, 2100), 10),
+    spec: {
+      weight: BAR_WEIGHT,
+    },
+    images: [
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-bar-valcambi-1kg-front.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-bar-valcambi-1kg-front.png`,
+      },
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-bar-valcambi-1kg-back.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-bar-valcambi-1kg-back.png`,
+      },
+    ],
+  },
+  {
+    id: faker.unique(v4),
     type: 'coin',
     metal: 'gold',
-    name: 'American Eagle',
-    slug: 'american-eagle',
+    name: '1 ounce Gold American Buffalo',
+    slug: '1oz-gold-buffalo',
     description: faker.lorem.lines(5),
     price: parseInt(faker.finance.amount(1200, 2100), 10),
     spec: {
@@ -43,12 +66,100 @@ export const storeProducts: Product[] = [
     },
     images: [
       {
-        thumbnail: `${PRODUCTS_IMAGES_PATH}/american-gold-eagle-1-ounce-front.png`,
-        fullSize: `${PRODUCTS_IMAGES_PATH}/american-gold-eagle-1-ounce-front.png`,
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-coin-buffalo-1oz-front.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-coin-buffalo-1oz-front.png`,
       },
       {
-        thumbnail: `${PRODUCTS_IMAGES_PATH}/american-gold-eagle-1-ounce-bac.png`,
-        fullSize: `${PRODUCTS_IMAGES_PATH}/american-gold-eagle-1-ounce-bac.png`,
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-coin-buffalo-1oz-back.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-coin-buffalo-1oz-back.png`,
+      },
+    ],
+  },
+  {
+    id: faker.unique(v4),
+    type: 'coin',
+    metal: 'gold',
+    name: '1 ounce Gold 1 ounce Silver Britannia',
+    slug: '1oz-gold-american-eagle',
+    description: faker.lorem.lines(5),
+    price: parseInt(faker.finance.amount(1200, 2100), 10),
+    spec: {
+      weight: OUNCE_WEIGHT,
+    },
+    images: [
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-coin-american-eagle-1oz-front.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-coin-american-eagle-1oz-front.png`,
+      },
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/gold-coin-american-eagle-1oz-back.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/gold-coin-american-eagle-1oz-back.png`,
+      },
+    ],
+  },
+  {
+    id: faker.unique(v4),
+    type: 'bar',
+    metal: 'silver',
+    name: '1kg Silver Bar - JBR',
+    slug: '1kg-silver-bar-JBR',
+    description: faker.lorem.lines(5),
+    price: parseInt(faker.finance.amount(1200, 2100), 10),
+    spec: {
+      weight: OUNCE_WEIGHT,
+    },
+    images: [
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-bar-JBR-1kg.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-bar-JBR-1kg.png`,
+      },
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-bar-JBR-1kg.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-bar-JBR-1kg.png`,
+      },
+    ],
+  },
+  {
+    id: faker.unique(v4),
+    type: 'coin',
+    metal: 'silver',
+    name: '1 ounce Silver Philharmonic',
+    slug: '1oz-silver-philharmonic',
+    description: faker.lorem.lines(5),
+    price: parseInt(faker.finance.amount(1200, 2100), 10),
+    spec: {
+      weight: OUNCE_WEIGHT,
+    },
+    images: [
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-coin-philharmonic-1oz-front.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-coin-philharmonic-1oz-front.png`,
+      },
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-coin-philharmonic-1oz-back.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-coin-philharmonic-1oz-back.png`,
+      },
+    ],
+  },
+  {
+    id: faker.unique(v4),
+    type: 'coin',
+    metal: 'silver',
+    name: '1 ounce Silver Britannia',
+    slug: '1oz-silver-britannia',
+    description: faker.lorem.lines(5),
+    price: parseInt(faker.finance.amount(1200, 2100), 10),
+    spec: {
+      weight: OUNCE_WEIGHT,
+    },
+    images: [
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-coin-britannia-1oz-front.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-coin-britannia-1oz-front.png`,
+      },
+      {
+        thumbnail: `${PRODUCTS_IMAGES_PATH}/silver-coin-britannia-1oz-back.png`,
+        fullSize: `${PRODUCTS_IMAGES_PATH}/silver-coin-britannia-1oz-back.png`,
       },
     ],
   },
