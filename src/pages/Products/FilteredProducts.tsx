fb5322f49c100afa1816341a78893b3457aac9a2
matcha <massahiro.endo@gmail.com>
2022-08-20 00:23:57 +0200

    feat: filtered items list
    to display on the products list page applying the multi filter tool chaining .filter(à.filter().map()

---
+++ src/pages/Products/FilteredProducts.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { StringOrNumber } from '@chakra-ui/utils';
+
+import { Product } from '../../constants/products';
+import ProductCard from './ProductCard';
+
+type FilteredProductsProps = {
+  products: Product[];
+  metals: StringOrNumber[];
+  categories: StringOrNumber[];
+};
+
+const filterByMetal = (metals: StringOrNumber[]) => (product: Product) => {
+  if (metals.length === 0) {
+    return true;
+  }
+  return metals.includes(product.metal);
+};
+const filterByCategory = (categories: StringOrNumber[]) => (product: Product) => {
+  if (categories.length === 0) {
+    return true;
+  }
+  return categories.includes(product.category);
+};
+
+const FilteredProducts: FC<FilteredProductsProps> = ({ products, metals, categories }) => {
+  return (
+    <Grid
+      gridTemplateColumns={{
+        base: 'repeat(1, 1fr)',
+        md: 'repeat(2, 1fr)',
+        xl: 'repeat(3, 1fr)',
+      }}
+      gap={1}>
+      {products
+        .filter(filterByMetal(metals))
+        .filter(filterByCategory(categories))
+        .map((p: Product) => (
+          <GridItem p={7} justifySelf="center">
+            <ProductCard product={p} key={p.id} />
+          </GridItem>
+        ))}
+    </Grid>
+  );
+};
+
+export default FilteredProducts;
