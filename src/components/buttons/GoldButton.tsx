326369deafe08e6df62c06813fe575cbceca5037
matcha <massahiro.endo@gmail.com>
2022-08-21 00:47:18 +0200

    feat: _hover & _focus props directly in buttons
    #18

--- src/components/buttons/GoldButton.tsx
+++ src/components/buttons/GoldButton.tsx
@@ -2,14 +2,14 @@ import { ButtonProps } from '@chakra-ui/react';
 import { FC, PropsWithChildren } from 'react';
 import MetalButton from './MetalButton';
 
-const GoldButton: FC<PropsWithChildren<ButtonProps>> = (props) => {
+const GoldButton: FC<PropsWithChildren<ButtonProps>> = ({ children, _focus, _hover, ...rest }) => {
   return (
     <MetalButton
       borderColor="gold.700"
-      _hover={{ backgroundColor: 'rgba(240, 215, 165, 0.5)' }}
-      _focus={{ boxShadow: '0 0 0 3px rgba(240, 215, 165, 0.5)' }}
-      {...props}>
-      {props.children}
+      _hover={{ backgroundColor: 'rgba(240, 215, 165, 0.5)', ..._hover }}
+      _focus={{ boxShadow: '0 0 0 3px rgba(240, 215, 165, 0.5)', ..._focus }}
+      {...rest}>
+      {children}
     </MetalButton>
   );
 };
--- src/components/buttons/SilverButton.tsx
+++ src/components/buttons/SilverButton.tsx
@@ -2,14 +2,14 @@ import { ButtonProps } from '@chakra-ui/react';
 import { FC, PropsWithChildren } from 'react';
 import MetalButton from './MetalButton';
 
-const SilverButton: FC<PropsWithChildren<ButtonProps>> = (props) => {
+const SilverButton: FC<PropsWithChildren<ButtonProps>> = ({ children, _focus, _hover, ...rest }) => {
   return (
     <MetalButton
       borderColor="#bdbdbd"
-      _hover={{ backgroundColor: 'rgba(189, 189, 189, 0.2)' }}
-      _focus={{ boxShadow: '0 0 0 3px rgba(189, 189, 189, 0.2)' }}
-      {...props}>
-      {props.children}
+      _hover={{ backgroundColor: 'rgba(189, 189, 189, 0.2)', ..._hover }}
+      _focus={{ boxShadow: '0 0 0 3px rgba(189, 189, 189, 0.2)', ..._focus }}
+      {...rest}>
+      {children}
     </MetalButton>
   );
 };
