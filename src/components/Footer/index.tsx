d9da267dfe1e4db60fc5d346a6b6beb1e524ae9c
matcha <massahiro.endo@gmail.com>
2022-08-21 12:13:22 +0200

    feat: content in footer area
    feature copy to clipboard onclick on shareIcon

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,121 @@
-import { Box, useColorModeValue } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
+import {
+  Badge,
+  Box,
+  Button,
+  Container,
+  Divider,
+  HStack,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+  useClipboard,
+  useColorModeValue,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+import { VscGithub } from 'react-icons/vsc';
+import { BsFillShareFill } from 'react-icons/bs';
 
-import { COLORS } from '../../constants/colors';
+import json from '../../../package.json';
+import GoldLingotIcon from '../../components/icons/GoldLingotIcon';
+import { COLORS } from '../../theme/colors';
+
+type FooterButtonProps = { label: string; href: string; onClick?: () => void; target?: string };
+
+const FooterButton: FC<PropsWithChildren<FooterButtonProps>> = ({ children, label, href, onClick, target = '' }) => {
+  return (
+    <Button
+      bg="#334D5C"
+      onClick={onClick}
+      rounded={'full'}
+      w={10}
+      h={10}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      target={target}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      size="full"
+      _hover={{ bg: 'gray.700' }}>
+      {children}
+    </Button>
+  );
+};
 
 const Footer: FC = () => {
-  const bg = useColorModeValue(COLORS.GOLD, COLORS.SILVER);
+  const { onCopy } = useClipboard(json.homepage);
+  const bg = useColorModeValue(COLORS.GOLD, COLORS.DARK);
+  const iconColor = useColorModeValue(COLORS.SILVER, COLORS.GOLD);
+  const copyToast = useToast({
+    title: 'Site URL Copied!',
+    status: 'success',
+    position: 'top',
+    isClosable: true,
+    description: (
+      <>
+        You can now paste {json.homepage} in your favorite sns app with <Badge>cmd+v</Badge> on mac or{' '}
+        <Badge>ctrl+v</Badge> on pc
+      </>
+    ),
+  });
+
+  const handleCopyButtonClick = () => {
+    onCopy();
+    copyToast();
+  };
+
   return (
-    <footer>
-      <Box bg={bg} p={4}>
-        Pied de nez
-      </Box>
+    <footer style={{ backgroundColor: bg, paddingTop: '12px' }}>
+      <Container as={Stack} maxW={'6xl'} py={4} spacing={4} justify={'center'} align={'center'}>
+        <Box display="flex" gap={4}>
+          <GoldLingotIcon fontSize={'2xl'} /> Official {json.name.toLocaleUpperCase()} App
+        </Box>
+        <VStack>
+          <Text>
+            Site powered with{' '}
+            <Link href="https://reactjs.org/" target="_blank">
+              React JS v18.0.8
+            </Link>
+          </Text>
+          <Text>
+            and{' '}
+            <Link href="https://chakra-ui.com/" target="_blank">
+              Chakra-ui
+            </Link>{' '}
+            components library
+          </Text>
+        </VStack>
+      </Container>
+      <Divider />
+      <Container
+        as={Stack}
+        maxW={'3xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>© {new Date().getFullYear()} All rights not reserved</Text>
+        <HStack spacing={6}>
+          <FooterButton label={'Github repository'} href={json.url} target="_blank">
+            <VscGithub color={'white'} />
+          </FooterButton>
+          <IconButton
+            bg="#334D5C"
+            _hover={{ bg: 'gray.700' }}
+            aria-label={`Share ${json.name} site url with others`}
+            rounded="full"
+            onClick={handleCopyButtonClick}
+            transition={'background 0.3s ease'}
+            icon={<BsFillShareFill color={iconColor} />}
+          />
+        </HStack>
+      </Container>
     </footer>
   );
 };
