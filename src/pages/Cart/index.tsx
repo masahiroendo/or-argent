c6b3066999c2ed39e0720978471ffff77eb281a9
matcha <massahiro.endo@gmail.com>
2022-08-29 13:25:48 +0200

    feat: Cart Component & Route

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,17 +1,8 @@
 import { Table } from '@chakra-ui/react';
 import { FC } from 'react';
-import { useParams } from 'react-router-dom';
-import RecommendedProductsCarousel from '../ProductDetails/RecommendedProductsCarousel';
 
-const Cart: FC = () => {
-  const { slug } = useParams();
-
-  return (
-    <>
-      <Table>Cart Page</Table>
-      {/* <RecommendedProductsCarousel metal={metal} slug={slug} /> */}
-    </>
-  );
+const CartPage: FC = () => {
+  return <Table>Cart Page</Table>;
 };
 
-export default Cart;
+export default CartPage;
