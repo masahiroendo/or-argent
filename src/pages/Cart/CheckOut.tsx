3ff6335ee05c384bdd6b65fc06f7557fa73f5883
matcha <massahiro.endo@gmail.com>
2022-09-02 22:32:20 +0200

    feat : alphabetic RegEx & email Regex impl

--- src/pages/Cart/CheckOut.tsx
+++ src/pages/Cart/CheckOut.tsx
@@ -22,20 +22,34 @@ import CartSummary from './CartSummary';
 import PaypalCheckout from './PaypalCheckout';
 import StripeCheckout from './StripeCheckout';
 
+const alphabeticRegex = new RegExp(/^[A-Za-z]+$/);
+const emailRegex = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
 const zipCodeRegex = new RegExp(/^\d{5}(?:[-\s]\d{4})?$/);
 
 const CheckOut = () => {
   const { signedIn, me } = useAuth();
   const { t } = useTranslation(['translation', 'cart']);
   const [paymentMethod, setPaymentMethod] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
   const [zipCode, setZipcode] = useState('');
 
   const handleZipCodeChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setZipcode(event.target.value);
   };
 
+  const handleFirstNameChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setEmail(event.target.value);
+  };
+
   // const isZipCodeInvalid = zipCode !== '' && zipCode.trim().length !== 5;
   const isZipCodeInvalid = zipCode !== '' && !zipCodeRegex.test(zipCode);
+  const isFirstNameInvalid = firstName !== '' && !alphabeticRegex.test(firstName);
+  const isEmailInvalid = email !== '' && !emailRegex.test(email);
 
   return (
     <Container my={5} maxW="8xl">
@@ -45,18 +59,26 @@ const CheckOut = () => {
             <Heading>{t('personal-info', { ns: 'cart' })}</Heading>
             <Box>
               <HStack>
-                <FormControl isRequired>
+                <FormControl isRequired onChange={handleFirstNameChange} isInvalid={isFirstNameInvalid}>
                   <FormLabel htmlFor="firstname">{t('firstname')}</FormLabel>
-                  <Input type="text" name="firstname" id="firstname" placeholder={`${t('firstname')}`} />
+                  <Input
+                    type="text"
+                    value={firstName}
+                    name="firstname"
+                    id="firstname"
+                    placeholder={`${t('firstname')}`}
+                  />
+                  <FormErrorMessage>{t('first-name-error-message', { ns: 'cart' })} </FormErrorMessage>
                 </FormControl>
                 <FormControl isRequired>
                   <FormLabel htmlFor="lastname">{t('lastname')}</FormLabel>
                   <Input type="text" name="lastname" id="lastname" placeholder={`${t('lastname')}`} />
                 </FormControl>
               </HStack>
-              <FormControl isRequired>
+              <FormControl isRequired onChange={handleEmailChange} isInvalid={isEmailInvalid}>
                 <FormLabel htmlFor="email">{t('email')}</FormLabel>
-                <Input type="email" id="email" name="email" placeholder={`${t('email')}`} />
+                <Input value={email} type="email" id="email" name="email" placeholder={`${t('email')}`} />
+                <FormErrorMessage>{t('email-error-message', { ns: 'cart' })}</FormErrorMessage>
               </FormControl>
             </Box>
             <Heading>{t('shipping-address', { ns: 'cart' })}</Heading>
